454511a5ee9392a7d6319670b66ec5839753e748
David <hsienwu830808@gmail.com>
2019-12-21 22:13:53 +0800

    Add turtle graph

---
+++ Day01-15/Day01/turtleProject.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle.pensize(10)
+turtle.pencolor('red')
+    
+turtle.forward(100)
+turtle.right(90)  # angle 90
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+
+turtle.mainloop()

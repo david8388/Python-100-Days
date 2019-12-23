5bf5a0c1bf2561c77378bfce74715a7ebc61d970
David <hsienwu830808@gmail.com>
2019-12-24 07:33:03 +0800

    Add second answer in piecewise.py

--- Day01-15/Day03/piecewise.py
+++ Day01-15/Day03/piecewise.py
@@ -1,5 +1,6 @@
 x = float(input('x = '))
 
+# first
 if x > 1:
     y = 3 * x - 5
 elif 1 >= x and x >= -1:
@@ -8,4 +9,16 @@ else:
     y = 5 * x + 3
 
 
-print('f(%.2f) = %.2f' % (x, y))
+# second
+if x > 1:
+    y2 = 3 * x - 5
+else: 
+    if x >= -1:
+        y2 = x + 2
+    else:
+        y2 = 5 * x + 3
+
+
+
+print('First：f(%.2f) = %.2f' % (x, y))
+print('Second：f(%.2f) = %.2f' % (x, y2))

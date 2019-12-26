f0fecc8ba74b873a3e71882e86c7bab5ee471821
David <hsienwu830808@gmail.com>
2019-12-27 06:50:24 +0800

    Add function1.py

---
+++ Day01-15/function1.py
@@ -0,0 +1,16 @@
+m = int(input('m = '))
+n = int(input('n = '))
+
+fm = 1
+for num in range(1, m + 1):
+    fm *= num
+
+fn = 1
+for num in range(1, n + 1):
+    fn *= num
+
+fmn = 1
+for num in range(1, m - n + 1):
+    fmn *= 1
+
+print(fm // fn // fmn)

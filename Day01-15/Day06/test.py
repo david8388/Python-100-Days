9710c5a084b25e8c72ecdc8a2bb30d95a265770e
David <hsienwu830808@gmail.com>
2019-12-28 21:51:45 +0800

    import module as alias

--- Day01-15/Day06/test.py
+++ Day01-15/Day06/test.py
@@ -1,6 +1,12 @@
-from module1 import foo
+# from module1 import foo
 
-foo() # hello world
+# foo() # hello world
 
-from module2 import foo
-foo() # goodbye world
+# from module2 import foo
+# foo() # goodbye world
+
+import module2 as m2
+import module1 as m1
+
+m1.foo()
+m2.foo()

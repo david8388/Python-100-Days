89494e902e54a9a8e69b96844f9599323b61034b
David <hsienwu830808@gmail.com>
2019-12-31 07:25:48 +0800

    Use sort function by list

--- Day01-15/Day07/list3.py
+++ Day01-15/Day07/list3.py
@@ -9,3 +9,14 @@ fruits4 = fruits[-3: -1]
 print(fruits4)
 fruits5 = fruits[::-1]
 print(fruits5)
+
+list = ['orange', 'apple', 'zoo', 'internationalization', 'blueberry']
+list2 = sorted(list)
+list3 = sorted(list, reverse = True)
+list4 = sorted(list, key = len)
+print(list2)
+print(list3)
+print(list4)
+
+list.sort(reverse = True)
+print(list)

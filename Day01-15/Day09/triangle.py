00761e645ae5ea3f9efcc67670dc811631060126
David <hsienwu830808@gmail.com>
2020-01-04 08:48:26 +0800

    Add triangle.py

---
+++ Day01-15/Day09/triangle.py
@@ -0,0 +1,34 @@
+from math import sqrt
+
+
+class Triangle(object):
+
+    def __init__(self, a, b, c):
+        self._a = a
+        self._b = b
+        self._c = c
+
+    @staticmethod
+    def is_valid(a, b, c):
+        return a + b > c and a + c > b and c + b > a
+
+    def perimeter(self):
+        return self._a + self._b + self._c
+
+    def area(self):
+        half = self.perimeter() / 2
+        return sqrt(half * (half - self._a) * (half - self._b) * (half - self._c))
+
+def main():
+    a, b, c = 3, 4, 5
+
+    if Triangle.is_valid(a, b, c):
+        t = Triangle(a, b, c)
+        print(t.perimeter())
+        print(t.area())
+    else:
+        print('無法構成三角形')
+
+
+if __name__ == '__main__':
+    main()

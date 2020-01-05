eb8d98ec4a5368cf12f3d0ccd7ff3201b656e8af
David <hsienwu830808@gmail.com>
2020-01-05 09:28:32 +0800

    Add pet.py

---
+++ Day01-15/Day09/pet.py
@@ -0,0 +1,31 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Pet(object, metaclass=ABCMeta):
+
+    def __init__(self, nickname):
+        self._nickname = nickname
+
+    @abstractmethod
+    def make_voice(self):
+        pass
+
+
+class Dog(Pet):
+    def make_voice(self):
+        print('%s: 旺旺旺' % self._nickname)
+
+
+class Cat(Pet):
+    def make_voice(self):
+        print('%s: 喵..喵..' % self._nickname)
+
+
+def main():
+    pets = [Dog('旺財'), Cat('凱蒂'), Dog('小黑')]
+    for pet in pets:
+        pet.make_voice()
+
+
+if __name__ == '__main__':
+    main()

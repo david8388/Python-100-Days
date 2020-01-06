75c3064eca1c638946e2edf24c2cb1a0ec2c9a2a
David <hsienwu830808@gmail.com>
2020-01-06 21:46:47 +0800

    Rename output file name

--- Day01-15/Day11/binary.py
+++ Day01-15/Day11/binary.py
@@ -3,7 +3,7 @@ def main():
         with open('2020_avg.jpeg', 'rb') as fs1:
             data = fs1.read()
             print(type(data)) # <class 'bytes'>
-        with open('2020_avg.jpeg', 'wb') as fs2:
+        with open('2020_avg_copy.jpeg', 'wb') as fs2:
             fs2.write(data)
     except FileNotFoundError as e:
         print('指定文件無法打開')

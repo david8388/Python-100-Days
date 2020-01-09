2916cf3bca0dad07590252375fb88bd7968b87cc
David <hsienwu830808@gmail.com>
2020-01-10 06:35:55 +0800

    Add multithread2.py

---
+++ Day01-15/Day13/multithread2.py
@@ -0,0 +1,32 @@
+from random import randint
+from threading import Thread
+from time import time, sleep
+
+
+class DownloadTask(Thread):
+
+    def __init__(self, filename):
+        super().__init__()
+        self._filename = filename
+
+    def run(self):
+        print('開始下載%s...' % self._filename)
+        time_to_download = randint(5, 10)
+        sleep(time_to_download)
+        print('%s下載完成, 耗費%d秒' % (self._filename, time_to_download))
+
+
+def main():
+    start = time()
+    t1 = DownloadTask('Python101')
+    t1.start()
+    t2 = DownloadTask('Peking Hot.avi')
+    t2.start()
+    t1.join()
+    t2.join()
+    end = time()
+    print('總共花費%.2f秒' % (end - start))
+
+
+if __name__ == '__main__':
+    main()

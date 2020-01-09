7c0ee606b241d17369c603f228c4e5b995f80df9
David <hsienwu830808@gmail.com>
2020-01-10 06:51:59 +0800

    Add lock to protect variable

--- Day01-15/Day13/multithread3.py
+++ Day01-15/Day13/multithread3.py
@@ -1,16 +1,21 @@
 from time import sleep
-from threading import Thread
+from threading import Thread, Lock
 
 
 class Account(object):
 
     def __init__(self):
         self._balance = 0
+        self._lock = Lock()
 
     def deposit(self, money):
-        new_balance = self._balance + money
-        sleep(0.01)
-        self._balance = new_balance
+        self._lock.acquire()
+        try:
+            new_balance = self._balance + money
+            sleep(0.01)
+            self._balance = new_balance
+        finally:
+            self._lock.release()
 
     @property
     def balance(self):

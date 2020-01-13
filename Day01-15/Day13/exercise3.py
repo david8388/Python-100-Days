967688b1024752330c8a5d217161917c7bc6655f
David <hsienwu830808@gmail.com>
2020-01-13 20:45:05 +0800

    Use multiprocess to calculate result

--- Day01-15/Day13/exercise3.py
+++ Day01-15/Day13/exercise3.py
@@ -1,14 +1,35 @@
+from multiprocessing import Process, Queue
+from random import randint
 from time import time
 
-def main():
+
+def task_handler(current_list, result_queue):
     total = 0
-    number_list = [x for x in range(1, 100000001)]
-    start = time()
-    for number in number_list:
+    for number in current_list:
         total += number
+    result_queue.put(total)
+
+
+def main():
+    process = []
+    number_list = [x for x in range(1, 100000001)]
+    result_queue = Queue()
+    index = 0
+    for _ in range(8):
+        p = Process(target=task_handler,
+                    args=(number_list[index: index + 12500000], result_queue))
+        index += 12500000
+        process.append(p)
+        p.start()
+    start = time()
+    for p in process:
+        p.join()
+    total = 0
+    while not result_queue.empty():
+        total += result_queue.get()
     print(total)
     end = time()
-    print('Execution time: %.3fs' % (end - start))
+    print('Execution time: ', (end - start), 's', sep='')
 
 
 if __name__ == '__main__':

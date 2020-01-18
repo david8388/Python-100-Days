6206c94e95c76e1305ce883044093962f1bda688
David <hsienwu830808@gmail.com>
2020-01-18 11:07:32 +0800

    Crop pic in pic1.py

--- Day01-15/Day15/pic1.py
+++ Day01-15/Day15/pic1.py
@@ -1,8 +1,14 @@
 from PIL import Image
 im = Image.open("2020_avg.jpeg")
 
-im.format = 'jpeg'
-im.resize((768, 1280))
-im.mode = 'RGB'
-print(im.format, im.mode, im.size)
-im.show()
+# im.format = 'jpeg'
+# im.resize((768, 1280))
+# m.mode = 'RGB'
+# print(im.format, im.mode, im.size)
+# im.show()
+
+rect = 10, 10, 310, 310
+im.crop(rect).show()
+
+
+

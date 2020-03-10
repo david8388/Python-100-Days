dce110a1534fd68173a7fe99834d4bb89e82961d
David <hsienwu830808@gmail.com>
2020-03-10 21:19:20 +0800

    Finish Hangman project

--- Project/Hangman/game.py
+++ Project/Hangman/game.py
@@ -1,23 +1,32 @@
 import random
 
 fruits = ['apple', 'mango', 'grape', 'guava', 'melon']
-chances = 7
 randomFruit = random.choice(fruits)
+chances = len(randomFruit) + 2
 starLength = len(randomFruit)
-starWord = ''
+word = ''
 isSuccess = False
 
 for _ in range(starLength):
-    starWord += '*'
+    word += '*'
 
 print('Guess the word! HINT: word is a name of a fruit')
-print(starWord)
+print(word)
 
-for _ in range(chances):
-    char = str(input('Enter a letter to guess: '))
+for attempt in range(chances):
+    guessWord = str(input('The {0} Try! \nEnter a letter to guess: '.format(attempt+1)))
     for i in range(starLength):
-        if randomFruit.find(char, i):
-            print(char, end='')
-        else:
-            print('*', end='')
-        print()
+        if guessWord == randomFruit[i]:
+            word = word[:i] + guessWord + word[i+1:]
+        elif not word[i].isalpha():
+            word = word[:i] + '*' + word[i+1:]
+        if word == randomFruit:
+            isSuccess = True
+            break
+    if isSuccess:
+        print('Congrats!! The word is {0}'.format(word))
+        break
+    if not isSuccess and attempt == chances - 1:
+        print('You lose! The word is {0}'.format(randomFruit))
+    else:
+        print(word)

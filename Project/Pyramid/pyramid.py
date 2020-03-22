781b0e3594e004ebc8c07129506fa6452094501a
David <hsienwu830808@gmail.com>
2020-03-22 20:35:20 +0800

    Add inverted triangle

--- Project/Pyramid/pyramid.py
+++ Project/Pyramid/pyramid.py
@@ -2,12 +2,21 @@ length = int(input('Enter the height of pyramid '))
 
 i = 0
 
+x = length
+
 
 def asterisk(times):
     return '*' * (1 + (times - 1) * 2)
 
 
+# triangle
 while i < length:
     i += 1
     spaceTimes = length - i
     print(' ' * spaceTimes, asterisk(i))
+
+# Inverted triangle
+while x > 0:
+    spaceTimes = length - x
+    print(' ' * spaceTimes, asterisk(x))
+    x -= 1

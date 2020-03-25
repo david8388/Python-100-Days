89477a713363528f17a68134405c4c5aeff2f359
David <hsienwu830808@gmail.com>
2020-03-25 22:09:38 +0800

    Add another format in tables.py

--- Project/TimesTables/tables.py
+++ Project/TimesTables/tables.py
@@ -2,3 +2,11 @@
 for i in range(2, 10):
     for x in range(1, 10):
         print('{} * {} = {}'.format(i, x, i * x))
+
+print('------------------------')
+
+for x in range(1, 10):
+    for i in range(2, 10):
+        print('{} * {} = {:2}  '.format(i, x, i * x), end='')
+    print()
+

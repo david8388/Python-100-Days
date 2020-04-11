59af46fa80099f4b44d66f6bab677b91138d19fe
David <hsienwu830808@gmail.com>
2020-04-11 15:26:51 +0800

    Add another answer in singleNumber1.py

--- Project/LeetCode/singleNumber1.py
+++ Project/LeetCode/singleNumber1.py
@@ -4,12 +4,22 @@ from typing import List
 # Memory Usage: 16.2 MB
 
 
+#class Solution:
+#    def singleNumber(self, nums: List[int]) -> int:
+#        for i in range(len(nums)):
+#            newNums = nums[:i] + nums[i+1:]
+#            if nums[i] not in newNums:
+#                return nums[i]
+
+# Runtime: 80 ms
+# Memory Usage: 16.3 MB
+
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        for i in range(len(nums)):
-            newNums = nums[:i] + nums[i+1:]
-            if nums[i] not in newNums:
-                return nums[i]
+        xor_result = 0
+        for i in nums:
+            xor_result ^= i
+        return xor_result
 
 
 problem = Solution()

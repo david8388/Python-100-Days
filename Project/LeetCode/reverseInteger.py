e7b39d24c4ba95c4f353f6a61cda93148c602b24
David <hsienwu830808@gmail.com>
2020-04-18 15:41:14 +0800

    Add reverseInteger.py

---
+++ Project/LeetCode/reverseInteger.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverse(self, x: int) -> str:
+        limit = pow(2, 31)
+        sign = 1 if x >= 0 else -1
+        result = int(str(abs(x))[::-1])
+        if result > (limit - 1) or result < (limit * -1):
+            return 0
+        return result * sign
+
+problem = Solution()
+
+print(problem.reverse(1534236469))

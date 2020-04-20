41cd22d444e8567a560f73a20d9ba8f141e0026e
David <hsienwu830808@gmail.com>
2020-04-20 22:14:55 +0800

    Add another solution in searchInsert.py

--- Project/LeetCode/searchInsert.py
+++ Project/LeetCode/searchInsert.py
@@ -3,13 +3,24 @@ from typing import List
 # Runtime: 52 ms
 # Memory Usage: 14.6 MB
 
+#class Solution:
+#    def searchInsert(self, nums: List[int], target: int) -> int:
+#        nums.append(target)
+#        nums.sort()
+#        return nums.index(target)
+
+# Runtime: 56 ms
+# Memory Usage: 14.4 MB
+
+
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        nums.append(target)
-        nums.sort()
-        return nums.index(target)
+        for i, el in enumerate(nums):
+            if target <= el:
+                return i
+        return len(nums)
 
 
 problem = Solution()
 
-print(problem.searchInsert([0, 1, 3], 0))
+print(problem.searchInsert([0, 1, 3], 5))

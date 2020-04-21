06428cb0c42e4725576a6ce4d61a59be9a5ab76f
David <hsienwu830808@gmail.com>
2020-04-21 21:23:29 +0800

    Add another solution in removeElement.py

--- Project/LeetCode/removeElement.py
+++ Project/LeetCode/removeElement.py
@@ -3,14 +3,25 @@ from typing import List
 # Runtime: 36 ms
 # Memory Usage: 14 MB
 
+#class Solution:
+#    def removeElement(self, nums: List[int], val: int) -> int:
+#        for i in range(len(nums) -1, -1, -1):
+#            if nums[i] == val:
+#                del nums[i]
+#        return len(nums)
+
+# Runtime: 36 ms
+# Memory Usage: 14 MB
+
+
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        for i in range(len(nums) -1, -1, -1):
-            if nums[i] == val:
-                del nums[i]
+        # https://blog.csdn.net/u010558281/article/details/53355211
+        for i in nums[:]:
+            if i == val:
+                nums.remove(i)
         return len(nums)
-
 
 problem = Solution()
 
-print(problem.removeElement([3, 2, 2, 3], 3))
+print(problem.removeElement([1, 2, 3, 3, 3, 4], 3))

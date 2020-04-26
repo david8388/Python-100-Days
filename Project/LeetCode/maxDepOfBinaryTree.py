1394afaf66c4657d27831bb29d4818cbc05b490c
David <hsienwu830808@gmail.com>
2020-04-26 14:11:28 +0800

    Refactor solution for maxDepOfBinaryTree.py

--- Project/LeetCode/maxDepOfBinaryTree.py
+++ Project/LeetCode/maxDepOfBinaryTree.py
@@ -13,14 +13,9 @@ class Solution:
         if root is None:
             return 0
         else:
-            leftHeight = 0
-            rightHeight = 0
-            if root.left is not None:
-                leftHeight = self.maxDepth(root.left)
-            if root.right is not None:
-                rightHeight = self.maxDepth(root.right)
-            height = leftHeight if leftHeight > rightHeight else rightHeight
-            return height + 1
+            leftHeight = self.maxDepth(root.left)
+            rightHeight = self.maxDepth(root.right)
+            return max(leftHeight, rightHeight) + 1
 
 
 bt = TreeNode(3)

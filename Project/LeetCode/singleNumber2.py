97f1b0bc3bafc2a53f538b7716fb91483eaf267c
David <hsienwu830808@gmail.com>
2020-04-26 10:01:27 +0800

    Add another solution in singleNumber2.py

--- Project/LeetCode/singleNumber2.py
+++ Project/LeetCode/singleNumber2.py
@@ -1,16 +1,29 @@
 from typing import List
+from collections import Counter
+
 
 # Runtime: 932 ms
 # Memory Usage: 15.3 MB
 
+# class Solution:
+#    def singleNumber(self, nums: List[int]) -> int:
+#        no = []
+#        for index, num in enumerate(nums):
+#            if num not in nums[index+1:] and num not in no:
+#                return num
+#            else:
+#                no.append(num)
+
+# Runtime: 56 ms
+# Memory Usage: 15.3 MB
+
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        no = []
-        for index, num in enumerate(nums):
-            if num not in nums[index+1:] and num not in no:
+        s = Counter(nums)
+        for index, num in enumerate(s):
+            if s[num] == 1:
                 return num
-            else:
-                no.append(num)
+
 
 problem = Solution()
 

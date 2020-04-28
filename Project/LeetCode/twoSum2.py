751e0034164aaf7a01b0dba81760b629be9faae1
David <hsienwu830808@gmail.com>
2020-04-28 20:24:42 +0800

    Add another solution in twoSum2.py

--- Project/LeetCode/twoSum2.py
+++ Project/LeetCode/twoSum2.py
@@ -1,20 +1,35 @@
 from typing import List
 
 
-# Runtime: 4028 ms
-# Memory Usage: 14.2 MB
-
 class Solution:
+
+    # Runtime: 4028 ms
+    # Memory Usage: 14.2 MB
+    # def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    #    for idx, num in enumerate(numbers):
+    #        aim = target - num
+    #        nextIdx = idx + 1
+    #        if aim in numbers[nextIdx:]:
+    #            aimIndex = numbers[nextIdx:].index(aim) + nextIdx
+    #            return [idx + 1, aimIndex + 1] # Not zero-based
+    #    return []
+
+    # Runtime: 64 ms
+    # Memory Usage: 14.4 MB
+
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        for idx, num in enumerate(numbers):
-            aim = target - num
-            nextIdx = idx + 1
-            if aim in numbers[nextIdx:]:
-                aimIndex = numbers[nextIdx:].index(aim) + nextIdx
-                return [idx + 1, aimIndex + 1] # Not zero-based
-        return []
+        left = 0
+        right = len(numbers) - 1
+        while left < right:
+            sum = numbers[left] + numbers[right]
+            if sum == target:
+                return [left + 1, right + 1]
+            elif sum > target:
+                right -= 1
+            else:
+                left += 1
 
 
 problem = Solution()
 
-print(problem.twoSum([2, 98, 0, 0, 3, 4], 0))
+print(problem.twoSum([0, 0, 2, 3, 4], 2))

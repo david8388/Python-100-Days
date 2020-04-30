51bd750e13756dedf2950b70b365cb9e60e074de
David <hsienwu830808@gmail.com>
2020-04-30 20:53:23 +0800

    Add another solution in findMin.py

--- Project/LeetCode/findMin.py
+++ Project/LeetCode/findMin.py
@@ -3,10 +3,24 @@ from typing import List
 # Runtime: 44 ms
 # Memory Usage: 11.4 MB
 
+# Using built-in function
+#class Solution:
+#    def findMin(self, nums: List[int]) -> int:
+#        return min(nums)
+
+# Runtime: 44 ms
+# Memory Usage: 14.3 MB
 
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        return min(nums)
+        first, last = 0, len(nums) - 1
+        while first < last:
+            mid = (first + last) // 2
+            if nums[mid] > nums[last]:
+                first = mid + 1
+            else:
+                last = mid
+        return nums[first]
 
 
 problem = Solution()

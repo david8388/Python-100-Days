1f374d4f3ce61ac838de24051bfc90cf7ae2a8d5
David <hsienwu830808@gmail.com>
2020-05-05 22:13:44 +0800

    Add solution in rightSideView.py

--- Project/LeetCode/rightSideView.py
+++ Project/LeetCode/rightSideView.py
@@ -1,5 +1,8 @@
 from typing import List
 
+# Runtime: 36 ms
+# Memory Usage: 13.8 MB
+# 199. Binary Tree Right Side View
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -7,26 +10,23 @@ class TreeNode:
         self.left = left
         self.right = right
 
+
 class Solution:
     def rightSideView(self, root: TreeNode) -> List[int]:
-        rightVal = []
-        cur = []
-        if root is not None:
-            if root.val:
-                rightVal.append(root.val)
-            if root.right:
-                cur = self.rightSideView(root.right)
-                rightVal.extend(cur)
-            elif root.left:
-                left = self.rightSideView(root.left)
-                rightVal.extend(left)
-        return rightVal
+        if root is None:
+            return []
+        rootVal = [root.val]
+        right = self.rightSideView(root.right)
+        left = self.rightSideView(root.left)
+        return rootVal + right + left[len(right):]
 
 
-#problem = Solution()
-#tn = TreeNode(1)
-#tn.left(TreeNode(2))
-#tn.right(TreeNode(3))
-#tn.left(TreeNode(2)).left(TreeNode(4))
-#print(tn)
-# print(problem.rightSideView(tn))
+problem = Solution()
+bt = TreeNode(1)
+bt.left = TreeNode(2)
+bt.right = TreeNode(3)
+bt.left.left = TreeNode(4)
+bt.left.right = TreeNode(5)
+bt.right.left = TreeNode(6)
+bt.right.right = TreeNode(7)
+print(problem.rightSideView(bt))
